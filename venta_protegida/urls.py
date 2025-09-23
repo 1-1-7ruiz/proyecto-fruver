66ce23bf0e02812a6f4980bbbd7dd1ce5e5fadb4
1-1-7ruiz <117estebanruiz@gmail.com>
2025-09-23 18:43:26 -0500

    :)

--- venta_protegida/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import * 
-
-urlpatterns=[
-    path('ventas-panel/<int:id>',class4.as_view()),
-    #path('categoria/<int:id>',classcategoria2.as_view())
-    
-]
